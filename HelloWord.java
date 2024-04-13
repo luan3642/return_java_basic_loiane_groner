a1aac03a0c39079442e6729cd304cc199595b478
luan3642 <luan3642@hotmail.com>
2024-04-12 23:50:04 -0300

    ...

--- HelloWord.java
+++ HelloWord.java
@@ -2,7 +2,7 @@ public class HelloWord{
 	
 	public static void main(String[] args){
 		for(int i = 0; i<=10; i++){
-			System.out.println("2"+ "X" + i + "="+ 2*i);
+			System.out.println("hello world");
 		}
 	}
 
