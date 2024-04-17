01a98256dd118f96b93275b5ddc99c79a3cdfa22
luan3642 <luan3642@hotmail.com>
2024-04-17 09:26:43 -0300

    ...

---
+++ java_loiane/java_basic/src/br/com/alldirect/Variaveis.java
@@ -0,0 +1,19 @@
+package br.com.alldirect;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		
+		
+		// convencao java
+		int idade;
+		String nome;
+		
+		
+		//proibido
+		int abstract;
+		
+		
+		
+		
+	}
+}
