3c5754d0b24f02bf1327abe4d5cd42db6b5d79ff
luan3642 <luan3642@hotmail.com>
2024-04-27 19:54:26 -0300

    ...

---
+++ java_loiane/java_basic/src/br/com/alldirect/OperadoresLogicos.java
@@ -0,0 +1,20 @@
+package br.com.alldirect;
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		
+		boolean verdade = true;
+		boolean falso = false;
+		
+		System.out.println(verdade && falso);
+		System.out.println(verdade || falso);
+		System.out.println(verdade ^ falso);
+		System.out.println(!verdade && falso);
+
+		
+		
+
+	}
+
+}
---
+++ java_loiane/java_basic/src/br/com/alldirect/basic/exercice/Ex1.java
@@ -0,0 +1,14 @@
+package br.com.alldirect.basic.exercice;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		print();
+		print();
+		
+	}
+	
+	
+	private static void print() {
+		System.out.println("HelloWorld");
+	}
+}
