aad652fa646f94f7f213c2f48c0cc0699350cfe3
luan3642 <luan3642@hotmail.com>
2024-06-02 16:06:36 -0300

    continuando no java

---
+++ java_loiane/java_basic/src/vetoresExercice/Ex1.java
@@ -0,0 +1,14 @@
+package vetoresExercice;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		int[] elementosA = {4,2,3,4,5};
+		int[] elementosB = elementosA;
+		
+		for(int elementos : elementosA) {
+			System.out.println(elementos);
+		}
+		
+		
+	}
+}
