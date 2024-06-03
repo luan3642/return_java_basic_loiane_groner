a1cfd60865d59563db6b59fe79602414f1291cf4
luan3642 <luan3642@hotmail.com>
2024-06-03 09:46:24 -0300

    continuando no java

---
+++ java_loiane/java_basic/src/br/com/matriz/Matriz.java
@@ -0,0 +1,51 @@
+package br.com.matriz;
+
+public class Matriz {
+	public static void main(String[] args) {
+		//iniciar matriz java
+		
+		int[][] matriz = new int[3][3];
+		
+		int[][] iniciandoValores = {
+				{1,2,3},
+				{2,3,6},
+				{7,8,9}
+		};
+		
+		//vamos acessar um indice desta matriz, passando linha e coluna
+		
+		System.out.println("acessando valor: [8]>>>>"+iniciandoValores[2][1]);
+		
+		
+		// percorrendo matriz
+		
+//		for(int i = 0; i<iniciandoValores.length; i++) {
+//			for(int j = 0; j<iniciandoValores[i].length; j++) {
+//				System.out.print(iniciandoValores[i][j]+ " ");
+//			}
+//			System.out.println();
+//		}
+		
+		//somar duas matrizes
+		
+		int[][] matriz1 = {
+				{1,2,3},
+				{4,5,6}
+				
+		};
+		
+		int[][] matriz2 = {
+				{2,5,6},
+				{5,7,9}
+		};
+		
+		int soma[][] = new int[2][3];
+		
+		for(int i = 0; i<matriz1.length; i++) {
+			for(int j = 0; j<matriz2[i].length; j++) {
+				soma[i][j] += matriz1[i][j] + matriz2[i][j]; 
+				System.out.println(soma[i][j]);
+			}
+		}
+	}
+}
