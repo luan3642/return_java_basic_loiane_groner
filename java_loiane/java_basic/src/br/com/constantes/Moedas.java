6de55102631a87ccabbb3e26ccef9511f3d5f443
luan3642 <luan3642@hotmail.com>
2024-07-04 14:55:15 -0300

    ...

---
+++ java_loiane/java_basic/src/br/com/constantes/Moedas.java
@@ -0,0 +1,7 @@
+package br.com.constantes;
+
+public enum Moedas {
+	PENNY(1), NICKEL(5), DIME(10), QUARTER(25);
+	
+	private int valor;
+}
---
+++ java_loiane/java_basic/src/br/com/loggs/ExemploLogger.java
@@ -0,0 +1,10 @@
+package br.com.loggs;
+
+import java.util.logging.Logger;
+
+public class ExemploLogger {
+	
+	//obtem o logger para classe atual
+	private static final Logger logger = Logger.getLogger(ExemploLogger.class.getName());
+
+}
